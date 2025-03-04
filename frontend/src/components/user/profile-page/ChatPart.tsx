4df05c4deba4dfc9b3f8afb8670911a55203e48f
Afsal Rahman <afsalrahman@gmail.com>
2025-03-04 10:56:03 +0530

    Bugs Solved on the Frontend

--- frontend/src/components/user/profile-page/ChatPart.tsx
+++ frontend/src/components/user/profile-page/ChatPart.tsx
@@ -234,18 +234,17 @@ const ChatPart = ({
                     >
                       <div className="mb-2">{msg.content}</div>
 
-                      {msg.image !== "no image" ||
-                        (!msg.image && (
-                          <div className="rounded-lg overflow-hidden">
-                            <img
-                              src={msg.image}
-                              alt="Message attachment"
-                              className="w-full max-w-[160px] min-w-[120px] object-cover rounded-lg hover:scale-105 transition-transform duration-200"
-                              loading="lazy"
-                              onClick={() => setSelectedImage(msg.image!)}
-                            />
-                          </div>
-                        ))}
+                      {msg.image == "no image" ? null : !msg.image ? null : (
+                        <div className="rounded-lg overflow-hidden">
+                          <img
+                            src={msg.image}
+                            alt="Message attachment"
+                            className="w-full max-w-[160px] min-w-[120px] object-cover rounded-lg hover:scale-105 transition-transform duration-200"
+                            loading="lazy"
+                            onClick={() => setSelectedImage(msg.image!)}
+                          />
+                        </div>
+                      )}
 
                       <p
                         className={`text-xs flex mt-2 ${
