a1e24d5fbb627a447ef469c9dc66e0b190cd5890
Afsal Rahman <afsalrahman@gmail.com>
2025-04-15 06:03:40 +0530

    Status code changed to Enums

--- backend/services/request-service/src/controllers/requestController.ts
+++ backend/services/request-service/src/controllers/requestController.ts
@@ -4,6 +4,7 @@ import IrequestController from "../interface/IrequestController";
 import requestService from "../services/requestService";
 import { validationResult } from "express-validator";
 import jwtFunctions from "../utils/jwt";
+import { StatusCode } from "../constants/statusCodes";
 
 export default class RequestController implements IrequestController {
   private _requestService: requestService;
@@ -17,12 +18,14 @@ export default class RequestController implements IrequestController {
       console.log("here reaching after the validation");
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
-        res.status(400).json({ errors: errors.array() });
+        res.status(StatusCode.OK).json({ errors: errors.array() });
         return;
       }
       const accessToken = req.headers["authorization"]?.split(" ")[1];
       if (!accessToken) {
-        res.status(400).json({ message: "No authorization token provided" });
+        res
+          .status(StatusCode.OK)
+          .json({ message: "No authorization token provided" });
       } else {
         const formData = req.body;
         const requestData = await this._requestService.insertRequest({
@@ -32,7 +35,7 @@ export default class RequestController implements IrequestController {
 
         res
           .setHeader("Authorization", `Bearer ${accessToken}`)
-          .status(200)
+          .status(StatusCode.OK)
           .json({ status: requestData.status, data: requestData.data });
       }
     } catch (error) {
@@ -48,12 +51,14 @@ export default class RequestController implements IrequestController {
     try {
       const accessToken = req.headers["authorization"]?.split(" ")[1];
       if (!accessToken) {
-        res.status(400).json({ message: "No authorization token provided" });
+        res
+          .status(StatusCode.OK)
+          .json({ message: "No authorization token provided" });
       } else {
         const requestData = await this._requestService.getRequests();
 
         res
-          .status(200)
+          .status(StatusCode.OK)
           .json({ status: requestData.status, data: requestData.data });
       }
     } catch (error) {
@@ -69,9 +74,9 @@ export default class RequestController implements IrequestController {
     try {
       const response = await this._requestService.getAllRedeemRequests();
       if (response.status) {
-        res.status(200).json(response);
+        res.status(StatusCode.OK).json(response);
       } else {
-        res.status(400).json(response);
+        res.status(StatusCode.OK).json(response);
       }
     } catch (error) {
       console.log("error in requestController", error);
@@ -82,13 +87,15 @@ export default class RequestController implements IrequestController {
     try {
       const accessToken = req.headers["authorization"]?.split(" ")[1];
       if (!accessToken) {
-        res.status(400).json({ message: "No authorization token provided" });
+        res
+          .status(StatusCode.OK)
+          .json({ message: "No authorization token provided" });
       } else {
         const { formData } = req.body;
         const responseData = await this._requestService.makePayment(formData);
         res
           .setHeader("Authorization", `Bearer ${accessToken}`)
-          .status(200)
+          .status(StatusCode.OK)
           .json({ status: responseData.status, data: responseData.data });
       }
     } catch (error) {
@@ -102,7 +109,7 @@ export default class RequestController implements IrequestController {
   ): Promise<void> => {
     try {
       if (!req.body.userRequests) {
-        res.status(400).json({
+        res.status(StatusCode.OK).json({
           status: false,
           message: "No User Requests found on the getUserRequests Post Request",
         });
@@ -112,9 +119,9 @@ export default class RequestController implements IrequestController {
         req.body.userRequests
       );
       if (response.status) {
-        res.status(200).json(response);
+        res.status(StatusCode.OK).json(response);
       } else {
-        res.status(400).json(response);
+        res.status(StatusCode.OK).json(response);
       }
     } catch (error) {
       console.log("error in requestController", error);
@@ -128,7 +135,7 @@ export default class RequestController implements IrequestController {
   ): Promise<void> => {
     try {
       if (!req.body.requestId) {
-        res.status(400).json({
+        res.status(StatusCode.OK).json({
           status: false,
           message: "Request Id not found on the getRequestDetails Post Request",
         });
@@ -136,14 +143,14 @@ export default class RequestController implements IrequestController {
       }
       const accessToken = req.headers["authorization"]?.split(" ")[1];
       if (!accessToken) {
-        res.status(400).json({
+        res.status(StatusCode.OK).json({
           status: false,
           message: "Access Token Not Found",
         });
       }
       const decoded = jwtFunctions.verifyAccessToken(accessToken!);
       if (!decoded) {
-        res.status(400).json({
+        res.status(StatusCode.OK).json({
           status: false,
           message: "Access Token Data Can't Get",
         });
@@ -155,15 +162,15 @@ export default class RequestController implements IrequestController {
         from: req.body.from,
       });
       if (response.status) {
-        res.status(200).json(response);
+        res.status(StatusCode.OK).json(response);
       } else {
         if (
           response.message == "Request Data not Found" ||
           response.message == "Invalid Request ID format"
         ) {
-          res.status(404).json(response);
+          res.status(StatusCode.NOT_FOUND).json(response);
         } else {
-          res.status(400).json(response);
+          res.status(StatusCode.OK).json(response);
         }
       }
     } catch (error) {
@@ -179,7 +186,7 @@ export default class RequestController implements IrequestController {
     try {
       const requestId = req.params.id;
       if (!requestId) {
-        res.status(400).json({
+        res.status(StatusCode.OK).json({
           status: false,
           message:
             "Request Id not found on the adminGetRequestDetails Get Request",
@@ -191,12 +198,12 @@ export default class RequestController implements IrequestController {
         requestId: requestId,
       });
       if (response.status) {
-        res.status(200).json(response);
+        res.status(StatusCode.OK).json(response);
       } else {
         if (response.message == "Invalid Request ID format") {
-          res.status(404).json(response);
+          res.status(StatusCode.NOT_FOUND).json(response);
         } else {
-          res.status(400).json(response);
+          res.status(StatusCode.OK).json(response);
         }
       }
     } catch (error) {
@@ -209,7 +216,9 @@ export default class RequestController implements IrequestController {
     try {
       const accessToken = req.headers["authorization"]?.split(" ")[1];
       if (!accessToken) {
-        res.status(400).json({ message: "No authorization token provided" });
+        res
+          .status(StatusCode.OK)
+          .json({ message: "No authorization token provided" });
       } else {
         const decoded = await jwtFunctions.verifyAccessToken(accessToken);
 
@@ -217,11 +226,11 @@ export default class RequestController implements IrequestController {
           requestId: req.body.requestId,
           userId: decoded?.id,
         });
-        res.status(200).json(response);
+        res.status(StatusCode.OK).json(response);
       }
     } catch (error) {
       res
-        .status(300)
+        .status(StatusCode.BAD_REQUEST)
         .json({ message: "error on the cancelRequest/requestController" });
     }
   };
@@ -238,7 +247,9 @@ export default class RequestController implements IrequestController {
       }
       const accessToken = req.headers["authorization"]?.split(" ")[1];
       if (!accessToken) {
-        res.status(400).json({ message: "No authorization token provided" });
+        res
+          .status(StatusCode.OK)
+          .json({ message: "No authorization token provided" });
       } else {
         const decoded = await jwtFunctions.verifyAccessToken(accessToken);
 
@@ -246,7 +257,7 @@ export default class RequestController implements IrequestController {
           requestId: req.body.requestId,
           userId: decoded?.id,
         });
-        res.status(200).json(response);
+        res.status(StatusCode.OK).json(response);
       }
     } catch (error) {
       console.log("error on the changeLikeStatus/requestController");
@@ -260,13 +271,15 @@ export default class RequestController implements IrequestController {
     try {
       const accessToken = req.headers["authorization"]?.split(" ")[1];
       if (!accessToken) {
-        res.status(400).json({ message: "No authorization token provided" });
+        res
+          .status(StatusCode.OK)
+          .json({ message: "No authorization token provided" });
       } else {
         const decoded = await jwtFunctions.verifyAccessToken(accessToken);
 
         if (!decoded) {
           res
-            .status(400)
+            .status(StatusCode.OK)
             .json({ message: "Unable to decode the Access Token" });
         } else {
           const response = await this._requestService.createRedeemRequest({
@@ -275,15 +288,15 @@ export default class RequestController implements IrequestController {
             userId: decoded.id,
           });
           if (response.status) {
-            res.status(200).json(response);
+            res.status(StatusCode.OK).json(response);
           } else {
-            res.status(400).json(response);
+            res.status(StatusCode.OK).json(response);
           }
         }
       }
     } catch (error) {
       res
-        .status(300)
+        .status(StatusCode.BAD_REQUEST)
         .json({ message: "error on the createRedeemRequest/adminController" });
     }
   };
@@ -295,28 +308,30 @@ export default class RequestController implements IrequestController {
     try {
       const accessToken = req.headers["authorization"]?.split(" ")[1];
       if (!accessToken) {
-        res.status(400).json({ message: "No authorization token provided" });
+        res
+          .status(StatusCode.OK)
+          .json({ message: "No authorization token provided" });
       } else {
         const decoded = await jwtFunctions.verifyAccessToken(accessToken);
 
         if (!decoded) {
           res
-            .status(400)
+            .status(StatusCode.OK)
             .json({ message: "Unable to decode the Access Token" });
         } else {
           const response = await this._requestService.getUserRedeemRequests({
             userId: decoded.id,
           });
           if (response.status) {
-            res.status(200).json(response);
+            res.status(StatusCode.OK).json(response);
           } else {
-            res.status(400).json(response);
+            res.status(StatusCode.OK).json(response);
           }
         }
       }
     } catch (error) {
       res
-        .status(300)
+        .status(StatusCode.BAD_REQUEST)
         .json({ message: "error on the createRedeemRequest/adminController" });
     }
   };
@@ -334,7 +349,7 @@ export default class RequestController implements IrequestController {
         requestRedeemId = req.params.id;
       }
       if (!requestRedeemId) {
-        res.status(401).json({
+        res.status(StatusCode.UNAUTHORIZED).json({
           message:
             "requestRedeemId no getting on the the getRedeemRequestDetails/adminController",
         });
@@ -343,17 +358,17 @@ export default class RequestController implements IrequestController {
           requestRedeemId,
         });
         if (response.status) {
-          res.status(200).json(response);
+          res.status(StatusCode.OK).json(response);
         } else {
           if (response.message == "Invalid Request ID format") {
-            res.status(404).json(response);
+            res.status(StatusCode.NOT_FOUND).json(response);
           } else {
-            res.status(400).json(response);
+            res.status(StatusCode.OK).json(response);
           }
         }
       }
     } catch (error) {
-      res.status(300).json({
+      res.status(StatusCode.BAD_REQUEST).json({
         message: "error on the getRedeemRequestDetails/adminController",
       });
     }
@@ -367,19 +382,21 @@ export default class RequestController implements IrequestController {
     try {
       const accessToken = req.headers["authorization"]?.split(" ")[1];
       if (!accessToken) {
-        res.status(400).json({ message: "No authorization token provided" });
+        res
+          .status(StatusCode.OK)
+          .json({ message: "No authorization token provided" });
       } else {
         const response = await this._requestService.changeRequestStatus(
           req.body.Props
         );
         res
           .setHeader("Authorization", `Bearer ${accessToken}`)
-          .status(200)
+          .status(StatusCode.OK)
           .json(response);
       }
     } catch (error) {
       res
-        .status(300)
+        .status(StatusCode.BAD_REQUEST)
         .json({ message: "error on the changeUserStatus/adminController" });
     }
   };
@@ -393,7 +410,7 @@ export default class RequestController implements IrequestController {
       console.log(req.body.Props);
       const { redeemRequestId, changeTo } = req.body.Props;
       if (!redeemRequestId || !changeTo) {
-        res.status(401).json({
+        res.status(StatusCode.UNAUTHORIZED).json({
           message:
             "Data are missing from the body getRedeemRequestDetails/adminController",
         });
@@ -404,13 +421,13 @@ export default class RequestController implements IrequestController {
       });
 
       if (response.status) {
-        res.status(200).json(response);
+        res.status(StatusCode.OK).json(response);
       } else {
-        res.status(400).json(response);
+        res.status(StatusCode.OK).json(response);
       }
     } catch (error) {
       res
-        .status(300)
+        .status(StatusCode.BAD_REQUEST)
         .json({ message: "error on the changeUserStatus/adminController" });
     }
   };
